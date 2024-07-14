2bd5ab10f63fd62f02ead9355d6652cc00160896
KiRa <cresciucmax@gmail.com>
2024-07-14 10:25:09 +0300

    main

--- SpJava/src/main/java/ks/msx/SpJava/utility/EmailSender.java
+++ SpJava/src/main/java/ks/msx/SpJava/utility/EmailSender.java
@@ -1,10 +1,15 @@
 package ks.msx.SpJava.utility;
 
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+
+import java.util.Properties;
 
 @Component
 @RequiredArgsConstructor
@@ -14,18 +19,41 @@ public class EmailSender{
     @Value("${spring.mail.username}")
     private String from;
 
+    @Value("${spring.mail.password}")
+    private String password;
+
     public void sendMSG(String to, String subject, String msg){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(msg);
+        Authenticator authenticator = new Authenticator() {
+            private PasswordAuthentication authentication;
+
+            {
+                authentication = new PasswordAuthentication(from, password);
+            }
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return authentication;
+            }
+        };
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props, authenticator);
+
         try {
-            emailSender.send(message);
-        }catch (Exception e){
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(from);
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(msg);
+            Transport.send(message);
+            System.out.println("sent");
+        }catch (MessagingException e){
             System.out.println(e);
         }
-        System.out.println("Message send");
     }
 
 }
